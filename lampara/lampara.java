9df9842f412c60eedc4a0815d11bad8717a3e9d3
Franck <202012672i@utea.edu.pe>
2025-06-23 11:26:51 -0500

    commit

--- lampara/lampara.java
+++ lampara/lampara.java
@@ -1,19 +1,26 @@
 package lampara;
 public class lampara {
-
     // Atributos
-        String color;
-        boolean encendida;
-        
-        // Constructor
-        public lampara(String color) {
-            this.color = color;
-            this.encendida = false;
-        }
-        
-        // Métodos
-        public void encender() {
-            this.encendida = true;
-            System.out.println("Lámpara encendida");
-        }
+    private String color;
+    private boolean encendida;
+    
+    // Constructor
+    public lampara(String color) {
+        this.color = color;
+        this.encendida = false;
+    }
+    
+    // Métodos
+    public void encender() {
+        this.encendida = true;
+        System.out.println("Lámpara encendida");
+    }
+    // Métodos públicos para acceder a los atributos (si es necesario)
+    public String getcolor() {
+        return color;
+    }
+
+    public boolean isencendida() {
+        return encendida;
+    }
 }
